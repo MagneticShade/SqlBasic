5f0b1e11e76517daed32202f8a629002e3ea7866
MagneticShade <senya.krivorot11@gmail.com>
2024-01-15 12:54:57 +0200

    mostly done

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Depends,UploadFile,HTTPException
+from fastapi import FastAPI,Depends,UploadFile,HTTPException,File
 from typing import Annotated
 from fastapi.middleware.cors import CORSMiddleware
 from db.database import engine,SessionLocal
@@ -7,6 +7,8 @@ import schemas.Img as schemas
 import models.Img as models
 from crud.crud import get_imges,add_imges
 import re
+import shutil
+from os import path
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -34,12 +36,23 @@ def img_all(db:Session=Depends(get_db)):
 
 @app.post('/',response_model=schemas.Img)
 def img_all(file:UploadFile,db:Session=Depends(get_db)):
-    size = len(file.file.read())
-    if(file.content_type == "image/png" and size<=(3*1024*1024)):
-        tmp=re.sub(".*/","",file.filename)
-        print (tmp)
-        img=schemas.Img(path=tmp)
-        imges=add_imges(db,img)
-        return imges
-    raise HTTPException(status_code=413)
+    file_content=file.file.read()
+    size = len(file_content)
+    if(file.content_type == "image/png"):
+
+        if( size<=(3*1024*1024)):
+            pure_name=re.sub(".*/","",file.filename)
+            
+            img=schemas.Img(path=pure_name)
+            imges=add_imges(db,img)
+            basepath = path.dirname(__file__)
+            file_location = path.abspath(path.join(basepath, "..","frontend","frontend","public", pure_name ))
+
+            with open(file_location, "wb+") as file_object:
+                file_object.write(file_content)
+                
+            return imges
+        raise HTTPException(status_code=413)
+    
+    raise HTTPException(status_code=412)
     
--- frontend/frontend/src/scripts/main.ts
+++ frontend/frontend/src/scripts/main.ts
@@ -1,32 +1,107 @@
-let app:HTMLElement = <HTMLElement>document.getElementById("app")
 
+import anime from 'animejs/lib/anime.es.js';
+
+let app:HTMLElement = <HTMLElement>document.getElementById("app")
+let fileInput:HTMLInputElement = <HTMLInputElement>document.getElementById("fileInput")
+let slides:[HTMLImageElement]
 type Img={
     path:string
 }
+
 function setStage(){
     app.replaceChildren(" ")
     fetch("http://127.0.0.1:8000/")
         .then(data=>data.json())
         .then(result=>{
             for (let img:Img of result){
-                let div:HTMLDivElement=document.createElement("div")
                 let imgBlock:HTMLImageElement=document.createElement("img")
-                div.appendChild(imgBlock)
                 imgBlock.src=img.path
-                app?.appendChild(div)
+                imgBlock.classList.add("slide")
+                app.appendChild(imgBlock)
+            }
+            slides=<[HTMLImageElement]>document.querySelectorAll(".slide")
+            for (let i=1;i<slides.length;i++){
+                slides[i].classList.add("hidden")
             }
         })
+
 }
+
 setStage()
-let fileInput:HTMLInputElement = <HTMLInputElement>document.getElementById("fileInput")
-let form:HTMLFormElement=<HTMLFormElement>document.querySelector("form")
+
+function ChangeSlide(){
+    let activeInd:number;
+    for (let i=0;i<slides.length;i++){
+        if (!slides[i].classList.contains("hidden")){
+            activeInd=i
+            break
+        }
+    }
+    slides[activeInd].classList.add("hidden")
+
+    if(activeInd==slides.length-1){
+        slides[0].classList.remove("hidden")
+        setAnimation(0)
+    }
+    else{
+        slides[activeInd+1].classList.remove("hidden")
+        setAnimation(activeInd+1)
+    }
+}
+
+function setAnimation(index:number){
+
+    let animNumber:number= Math.floor(Math.random() * (3 + 1));
+
+    switch (animNumber) {
+        case 0:
+            anime({
+                targets: slides[index],
+                translateX: 250,
+                direction:'reverse',
+                duration:250,
+                easing: 'linear',
+            })
+            break
+        case 1:
+            anime({
+                targets: slides[index],
+                translateY: 250,
+                direction:'reverse',
+                duration:250,
+                easing: 'linear',
+            })
+            break
+        case 2:
+            anime({
+                targets:slides[index],
+                direction:'reverse',
+                rotate:'1turn',
+                duration:250,
+                easing:'linear'
+            })
+            break
+        case 3:
+            anime({
+                targets:slides[index],
+                translateY:-250,
+                direction:'reverse',
+                duration:250,
+                easing:'linear'
+            })
+            break
+
+    }
+
+}
+
 fileInput?.addEventListener("change",()=>{
     if (fileInput.files){
         for (const file of fileInput.files){
-            console.log(file.name)
+
             let fdata=new FormData()
             fdata.append("file",file)
-            console.log(fdata.get("file"))
+
             fetch('http://127.0.0.1:8000/',{
                 method: "POST",
                 body:fdata})
@@ -34,3 +109,8 @@ fileInput?.addEventListener("change",()=>{
         }
     }
 })
+
+app.addEventListener("click",ChangeSlide)
+setInterval(()=>{
+    app.dispatchEvent(new Event("click"))
+},2000)
